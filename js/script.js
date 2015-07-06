eee57779d520a2a7ec8a7ad59b5ee374a5ac114d
Scott Faber <scottfaberphoto@gmail.com>
2015-07-06 10:03:26 -0400

    fizzbuzz as function sediotn 2-2-3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-for (var i = 1; i < 101; i++) {
-   if (i%3 + i%5 == 0) {
-        console.log('FizzBuzz');
-} else if (i%3 == 0) {
-        console.log('Fizz');
-} else if (i%5 == 0) {
-        console.log('Buzz');
-}  else {
-        console.log(i);
+//if not prompt, then use jQuery 
+var inputValue = prompt("What value do you want fizzbuzz to count to?");
+
+var inputValueNum = Number(inputValue);
+
+function fizzBuzzFunction(userNumber){
+    for (var i = 1; i < userNumber; i++) {
+       if (i%3 + i%5 == 0) {
+           //add DOM element containing "FizzBuzz"
+            console.log('FizzBuzz');
+    } else if (i%3 == 0) {
+        //add DOM element containing "Fizz"
+            console.log('Fizz');
+    } else if (i%5 == 0) {
+        //
+            console.log('Buzz');
+    }  else {
+            console.log(i);
+        }
+    }
 }
-}
+
+//examples of function calls with a single argument
+fizzBuzzFunction(inputValueNum);
+
+
